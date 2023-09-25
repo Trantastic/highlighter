2317d42ec00844cc72f37da6721494ab551abb9b
Yvette Tran <yvettetran@gmail.com>
2023-09-25 13:06:25 -0400

    removed consolelog line

--- highlight.js
+++ highlight.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousedown', async () => {
     if (getSelection()) {
         console.log("first condition");
+        // Only activate extension if not on a google doc page
         if (getSelection().anchorNode.id !== 'docs-title-widget') {
-            console.log("second condition");
             
             const range = document.getSelection().getRangeAt(0),
             span = document.createElement('span');
